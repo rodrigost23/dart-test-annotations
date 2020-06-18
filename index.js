8c00bc6347984dad383ef63c7d4851c62fdc406f
Rodrigo Tavares <rodrigost23@users.noreply.github.com>
2020-06-18 13:27:09 -0400

    Parsing json from path

--- index.js
+++ index.js
@@ -1,15 +1,85 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+const { GITHUB_TOKEN, GITHUB_WORKSPACE, INPUT_PATH } = process.env;
+
+async function main() {
+  try {
+    let fileContents = fs.readFileSync(core.getInput('path', { required: true }), { encoding: 'utf-8' });
+
+    console.log(parseOutput(fileContents));
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+/**
+ * @typedef {Object} ErrorItem
+ * @property {number} testID
+ * @property {string} stackTrace
+ * @property {string} error
+ * @property {string} type
+ * @property {bool} isFailure
+ */
+
+/**
+ * @param {string} output 
+ */
+function parseOutput(output) {
+  /** @type {(import("@octokit/types").ChecksListAnnotationsResponseData)[]} */
+  let annotations = [];
+
+  if (output.length == 0) {
+    return annotations;
+  }
+
+  let lines = output.trim().split(String.fromCharCode(10));
+
+  lines.forEach(line => {
+    /** @type {ErrorItem} */
+    let item = JSON.parse(line);
+
+    if (item.testID && item.error) {
+      let RegExp = /([\w\\\/\.]+)[ :](\d+):(\d+)/gi;
+
+      if (item.isFailure) {
+        let matches = RegExp.exec(item.stackTrace);
+        annotations.push({
+          path: matches[1],
+          start_line: matches[2],
+          end_line: matches[2],
+          start_column: matches[3],
+          end_column: matches[3],
+          annotation_level: "error",
+          message: item.error,
+          raw_details: item.stackTrace
+        });
+
+      } else if (item.type == "error") {
+        let err = item.error.replace(RegExp, "$$$$$$$&")
+        err.split("$$$").forEach(err => {
+          let matches = RegExp.exec(err);
+
+          if (matches) {
+            annotations.push({
+              path: matches[1],
+              start_line: matches[2],
+              end_line: matches[2],
+              start_column: matches[3],
+              end_column: matches[3],
+              annotation_level: "error",
+              message: err,
+              raw_details: item.stackTrace
+            });
+          }
+        });
+      }
+    }
+  });
+
+  return annotations;
+}
+
+main();
